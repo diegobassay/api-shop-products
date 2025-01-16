5dba67394715291a18e7a18ce8f30cbbc632642b
diego <mauricio.rocha@consultores.rnp.br>
2025-01-15 22:04:23 -0300

    implementado service client

---
+++ src/main/java/com/example/api_shop_products/service/ClientService.java
@@ -0,0 +1,44 @@
+package br.com.pereira.LojaDeDoces.services;
+
+import java.util.List;
+import java.util.regex.Pattern;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.api_shop_products.model.Client;
+import com.example.api_shop_products.repository.ClientRepository;
+//import br.com.pereira.LojaDeDoces.services.exception.IllegalArgumentException;
+//import br.com.pereira.LojaDeDoces.services.exception.ResourceNotFoundException;
+
+@Service
+public class ClientService {
+
+	@Autowired
+    private ClientRepository clientRepository;
+	
+	public List<Client> findAll() {
+		return clientRepository.findAll();
+	}
+	
+	public Client findById(Long id) {
+		return clientRepository.findById(id).orElseThrow(
+                () -> new ResourceNotFoundException()
+        );
+	}
+	
+	public Client findByCpf(String cpf) {
+		return clientRepository.findByCpf(cpf).orElseThrow(
+                () -> new ResourceNotFoundException()
+        );
+	}
+	
+	public void save(Client u) throws IllegalArgumentException {
+		clientRepository.save(u);
+	}
+
+	public void delete(Client u) throws IllegalArgumentException {
+		clientRepository.delete(u);
+	}
+	
+}
